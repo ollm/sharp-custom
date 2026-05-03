a4c64072291227e04d42ef568518b0b13bb0a24c
Oleguer Llopart <oleguer.llopart.mora@gmail.com>
2026-05-03 13:19:34 +0200

    test 1

---
+++ patch-build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+PLATFORM=$1
+
+# Replace @img/ with @img-custom/ in .json and .js only
+find . \
+  -path './.git' -prune -o \
+  -type f \( -name '*.json' -o -name '*.js' \) \
+  ! -name '*.bak' \
+  -exec sed -i.bak 's/@img\//@img-custom\//g' {} +
+
+
