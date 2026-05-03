672ff0ea87939b95091a81fdb52c422d623c5f50
Oleguer Llopart <oleguer.llopart.mora@gmail.com>
2026-05-03 17:51:56 +0200

    Upd: patch-common.sh

--- patch-common.sh
+++ patch-common.sh
@@ -1,21 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/sh
 set -e
-
-PLATFORM=$1
 
 # Replace @img/sharp with @img-custom/sharp in .json and .js only
 find . \
   \( -path './.git' -o -path '*/.git/*' \) -prune -o \
   -type f \( -name '*.json' -o -name '*.js' \) \
-  ! -name '*.bak' \
-  -exec sed -i.bak 's|@img/sharp-|@img-custom/sharp-|g' {} +
+  -exec sed -i 's|@img/sharp-|@img-custom/sharp-|g' {} +
 
-# Replace (lovell/sharp-libvips and lovell/sharp) with (ollm/sharp-libvips-custom and lovell/sharp-custom) in .json (For provenance)
+# Replace (lovell/sharp-libvips and lovell/sharp) with (ollm/sharp-libvips-custom and ollm/sharp-custom) in .json (For provenance)
 find . \
   \( -path './.git' -o -path '*/.git/*' \) -prune -o \
   -type f -name '*.json' \
-  ! -name '*.bak' \
-  -exec sed -i.bak \
+  -exec sed -i \
     -e 's|lovell/sharp-libvips|ollm/sharp-libvips-custom|g' \
     -e 's|lovell/sharp|ollm/sharp-custom|g' \
   {} +
@@ -24,12 +20,10 @@ find . \
 find . \
   \( -path './.git' -o -path '*/.git/*' \) -prune -o \
   -type f -name '*.json' \
-  ! -name '*.bak' \
-  -exec sed -i.bak 's|"0.34.5"|"0.0.2"|g' {} +
+  -exec sed -i 's|"0.34.5"|"0.0.2"|g' {} +
 
 # Replace version (Only for developing)
 find . \
   \( -path './.git' -o -path '*/.git/*' \) -prune -o \
   -type f -name '*.json' \
-  ! -name '*.bak' \
-  -exec sed -i.bak 's|"1.2.4"|"0.0.8"|g' {} +
+  -exec sed -i 's|"1.2.4"|"0.0.8"|g' {} +
