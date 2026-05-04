cb38aa79b20881f47895f393e3a75bf5191d445a
Oleguer Llopart <oleguer.llopart.mora@gmail.com>
2026-05-04 20:29:16 +0200

    test 21

--- patch-common.js
+++ patch-common.js
@@ -75,7 +75,7 @@ walk(ROOT, (file) => {
 			},
 			{	// Replace version (Only for developing): sharp-libvips
 				search: /"1\.3\.0-rc\.[0-9]+"/g,
-				replace: '"0.0.11"',
+				replace: '"1.3.0-rc.5"',
 			},
 			{	// Replace version (Only for developing): libvips
 				search: /8\.17\.3/g,
