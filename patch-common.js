96603a1bf5885dcb36cf63b3251af5de557ea960
Oleguer Llopart <oleguer.llopart.mora@gmail.com>
2026-05-10 12:10:03 +0200

    build with static libvips (Windows)

--- patch-common.js
+++ patch-common.js
@@ -50,156 +50,33 @@ walk(ROOT, (file) => {
 	if(!replace)
 		return;
 
-	const replacements = [];
-
-	// Replace @img/sharp with @img-custom/sharp
-	replacements.push({
-		search: /@img\/sharp-/g,
-		replace: '@img-custom/sharp-',
-	});
-
-	if(replace)
-	{
-		// TODO: Apply only to specific files and abort if they don't match; using this approach for now.
-		replacements.push(
-			{	// Replace lovell/sharp-libvips with ollm/sharp-libvips-custom in .json (For provenance)
-				search: /lovell\/sharp-libvips/g,
-				replace: 'ollm/sharp-libvips-custom',
-			},
-			{	// Replace lovell/sharp with ollm/sharp-custom in .json (For provenance)
-				search: /lovell\/sharp/g,
-				replace: 'ollm/sharp-custom',
-			},
-			{	// Add all Windows dll
-				search: /\(name\.startsWith\('libvips-'\)\s*&&\s*name\.endsWith\('\.dll'\)\);/g,
-				replace: '(name.startsWith(\'libvips-\') && name.endsWith(\'.dll\')) ||\n      (name.endsWith(\'.dll\'));',
-			},
-			{	// Test
-				search: /filter: \(file\) => \{/g,
-				replace: 'filter: (file) => {console.log(\'File:\', file);',
-			},
-			{	// Replace version (Only for developing): sharp
-				search: /"0\.35\.0-rc\.[0-9]+"/g,
-				replace: '"0.13.1"',
-			},
-			{	// Replace version (Only for developing): sharp-libvips
-				search: /"1\.3\.0-rc\.[0-9]+"/g,
-				replace: '"1.3.0-rc.6-5"',
-			},
-			{	// Replace version (Only for developing): libvips
-				search: /8\.17\.3/g,
-				replace: '8.18.2',
-			}
-		);
-	}
-
 	// TODO: Apply only to specific files and abort if they don't match; using this approach for now.
-	if(file.endsWith('.gyp'))
-	{
-		const lib = [
-			'libglib-2.0.lib',
-			'libgobject-2.0.lib',
-			'libgio-2.0.lib',
-
-			'libvips.lib',
-		];
-
-		const dll = [
-
-			'libc++.dll',
-			'libunwind.dll',
-			'libffi-8.dll',
-
-			'libz1.dll',
-			'libznz.dll',
-			'libzstd.dll',
-			'libbrotlicommon.dll',
-			'libbrotlidec.dll',
-			'libbrotlienc.dll',
-
-			'libexpat-1.dll',
-			'libxml2-16.dll',
-
-			'libglib-2.0-0.dll',
-			'libgobject-2.0-0.dll',
-			'libgmodule-2.0-0.dll',
-			'libgio-2.0-0.dll',
-
-			'libjpeg-62.dll',
-			'libpng16-16.dll',
-			'libtiff-6.dll',
-			'libuhdr.dll',
-			'libwebp-7.dll',
-			'libwebpdemux-2.dll',
-			'libwebpmux-3.dll',
-			'libsharpyuv-0.dll',
-
-			'liblcms2-2.dll',
-			'libfreetype-6.dll',
-			'libfontconfig-1.dll',
-			'libharfbuzz-0.dll',
-			'libfribidi-0.dll',
-
-			'libpixman-1-0.dll',
-			'libcairo-2.dll',
-
-			'libpango-1.0-0.dll',
-			'libpangoft2-1.0-0.dll',
-			'libpangocairo-1.0-0.dll',
-
-			'libopenjp2.dll',
-			'libjxl.dll',
-			'libjxl_threads.dll',
-			'libjxl_cms.dll',
-			'libhwy.dll',
-
-			'libheif.dll',
-			'libaom.dll',
-
-			'libimagequant.dll',
-			'libcgif-0.dll',
-			'libexif-12.dll',
-			'libmatio-14.dll',
-			'libniftiio.dll',
-			'libcfitsio.dll',
-			'libdicom-1.dll',
-			'libraw_r-24.dll',
-			// 'libopenslide-1.dll',
-
-			// 'libpoppler-158.dll',
-			// 'libpoppler-glib-8.dll',
-			'librsvg-2-2.dll',
-
-			'libarchive-13.dll',
-			'libsqlite3-0.dll',
-			'libfftw3-3.dll',
-
-			'libIex-3_1.dll',
-			'libIlmThread-3_1.dll',
-			'libOpenEXR-3_1.dll',
-
-			// 'libMagickCore-6.Q16-7.dll',
-
-			'libgdk_pixbuf-2.0-0.dll',
-
-			'libvips-42.dll',
-		];
-
-		replacements.push(
-			{
-				search: /(\['OS\s*==\s*"win"',\s*{\s*'defines':\s*\[\s*'_ALLOW_KEYWORD_MACROS',\s*'_FILE_OFFSET_BITS=64',\s*'_HAS_EXCEPTIONS=1'\s*\],\s*'link_settings':\s*{\s*'libraries':\s*\[\s*)[^\]\s]*/g,
-				replace: `$1${lib.map(name => `'${name}'`).join(',\n            ')}`,
-			},
-			{
-				search: /('library_dirs':\s*\[\s*'<\(sharp_libvips_lib_dir\)'\s*\],\s*'libraries':\s*\[\s*)[^\]\s]*/g,
-				replace: `$1${lib.map(name => `'${name}'`).join(',\n            ')}`,
-			},
-			{
-				search: /'\<\(sharp_libvips_lib_dir\)\/libvips-42\.dll'/g,
-				replace: dll.map(name => `'<(sharp_libvips_lib_dir)/${name}'`).join(',\n        '),
-			}
-		);
-	}
+	const replacements = [
+		{	// Replace @img/sharp with @img-custom/sharp
+			search: /@img\/sharp-/g,
+			replace: '@img-custom/sharp-',
+		},
+		{	// Replace lovell/sharp-libvips with ollm/sharp-libvips-custom in .json (For provenance)
+			search: /lovell\/sharp-libvips/g,
+			replace: 'ollm/sharp-libvips-custom',
+		},
+		{	// Replace lovell/sharp with ollm/sharp-custom in .json (For provenance)
+			search: /lovell\/sharp/g,
+			replace: 'ollm/sharp-custom',
+		},
+		{	// Replace version (Only for developing): sharp
+			search: /"0\.35\.0-rc\.[0-9]+"/g,
+			replace: '"0.14.0"',
+		},
+		{	// Replace version (Only for developing): sharp-libvips
+			search: /"1\.3\.0-rc\.[0-9]+"/g,
+			replace: '"1.3.0-rc.6-6"',
+		},
+		{	// Replace version (Only for developing): libvips
+			search: /8\.17\.3/g,
+			replace: '8.18.2',
+		}
+	];
 
 	processFile(file, replacements);
 
